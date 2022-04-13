95e8506a21e7b28aa01ed7d5bf2de4659459898d
DebanjanGhoshYudiz <debanjan.ghosh@yudiz.com>
2022-04-13 10:51:40 +0530

    Project Update!
    - Added New Script for Cube.
    - Now the Cube changes Colour.
    - Number are shown properly.

---
+++ Assets/Scripts/Core/Cube/CubeScript.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CubeScript : MonoBehaviour
+{
+    public int cubeNumber;
+    public TextMeshPro cubeText;
+    public SpriteRenderer cubeSpriteRenderer;
+    public Renderer tmproTextRenderer;
+
+    private void Start()
+    {
+        tmproTextRenderer.sortingOrder = 2;
+        cubeNumber = Random.Range(1, 100);
+        cubeText.text = cubeNumber.ToString();
+        CubeColorChange();
+    }
+
+    public void CubeColorChange()
+    {
+        if (cubeNumber <= 10)
+        {
+            cubeSpriteRenderer.color = Color.blue;
+        }
+        else if (cubeNumber > 10 && cubeNumber <= 30)
+        {
+            cubeSpriteRenderer.color = Color.green;
+        }
+        else if (cubeNumber > 30 && cubeNumber <= 50)
+        {
+            cubeSpriteRenderer.color = Color.red;
+        }
+        else
+        {
+            cubeSpriteRenderer.color = Color.cyan;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (cubeNumber >= 1)
+            {
+                cubeNumber--;
+                cubeText.text = cubeNumber.ToString();
+                CubeColorChange();
+            }
+            else if (cubeNumber == 0)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
